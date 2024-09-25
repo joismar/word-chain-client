6d3bfe1f420a0585841d7407d11cf22b9231ac71
cloudwilker <cloudwilker@gmail.com>
2024-09-25 19:00:32 +0000

    chore: fix es module

--- release.config.js
+++ release.config.js
@@ -1,17 +1,15 @@
-module.exports = {
-    branches: ['main'],
-    plugins: [
-      '@semantic-release/commit-analyzer', 
-      '@semantic-release/release-notes-generator',
-      '@semantic-release/changelog',
-      '@semantic-release/npm',
-      '@semantic-release/github',
-      [
+export const branches = ['main'];
+export const plugins = [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    '@semantic-release/npm',
+    '@semantic-release/github',
+    [
         '@semantic-release/git',
         {
-          assets: ['package.json', 'CHANGELOG.md'],
-          message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+            assets: ['package.json', 'CHANGELOG.md'],
+            message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
         }
-      ]
     ]
-  }
+];
